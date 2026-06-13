8c6730453b9df7c84aecf7ec8f1bc833c7a1ec3d
SlimeNull <slimenull@outlook.com>
2026-06-14 03:12:09 +0800

    release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.silmenull.communityesj"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
